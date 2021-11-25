f387e82b9721d82b21fdb15a9b77ab9e8c98b858
Amanda Carbone <amandamooney0418@gmail.com>
2021-11-24 23:07:25 -0500

    added responsive style for bigger screens, added view product buttons, made shopping cart icon an actual button, set up css classes for view product/checkout/receipt windows, set up event listeners for buttons

--- POSCyber.js
+++ POSCyber.js
@@ -1,1 +1,18 @@
-(function () {})();
+(function () {
+
+const viewProductButtons = document.querySelectorAll('.viewProductButton');
+const viewProductWindow = document.createElement('div');
+
+for(let button of viewProductButtons) {
+    button.addEventListener('click', event => {
+        viewProductWindow.classList.add('.viewProduct');
+        document.body.append('div');
+        
+    });
+};
+
+document.querySelector('.shoppingCartButton').addEventListener('click', event => {
+    console.log('It just works');
+})
+
+})();
